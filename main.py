6070190209f30c1b4a1083e0f15c61e5378380c0
mewhooik <sujalchavdaa@gmail.com>
2026-06-15 19:53:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,14 @@
 from fastapi import FastAPI
 import requests
 
-app = FastAPI(
-    title="ExamCrushers API",
-    version="1.0"
-)
-
-SOURCE_URL = "https://examcrushers.in/NT/proxy.php?endpoint=all-course"
-
+app = FastAPI()
 
 @app.get("/")
-def root():
-    return {
-        "status": "online",
-        "message": "API Running"
-    }
-
-
-@app.get("/health")
-def health():
-    return {
-        "status": "ok"
-    }
-
+def home():
+    return {"status": "ok"}
 
 @app.get("/allbatch")
 def allbatch():
-    try:
-        response = requests.get(
-            SOURCE_URL,
-            timeout=30
-        )
-
-        response.raise_for_status()
-
-        return response.json()
-
-    except Exception as e:
-        return {
-            "success": False,
-            "error": str(e)
-        }
+    return requests.get(
+        "https://examcrushers.in/NT/proxy.php?endpoint=all-course"
+    ).json()
